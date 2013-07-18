3aa54cebf46ccbc3a6831d3ac9e0f71eb01a45b7
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-07-18 14:38:00 +0200

    Add tests for going west.

--- test/test-coords.js
+++ test/test-coords.js
@@ -26,10 +26,24 @@ describe("coords", function () {
         });
     });
 
-    describe("se", function() {
+    describe("se", function () {
         it("should go south-east", function () {
             assert.deepEqual(coords.se({ x: 4, y: 2 }), { x: 5, y: 2 });
             assert.deepEqual(coords.se({ x: 1, y: 0 }), { x: 2, y: 1 });
+        });
+    });
+
+    describe("nw", function () {
+        it("should go north-west", function () {
+            assert.deepEqual(coords.nw({ x: 6, y: 1 }), { x: 5, y: 0 });
+            assert.deepEqual(coords.nw({ x: 1, y: 0 }), { x: 0, y: 0 });
+        });
+    });
+
+    describe("sw", function() {
+        it("should go south-west", function () {
+            assert.deepEqual(coords.sw({ x: 2, y: 2 }), { x: 1, y: 2 });
+            assert.deepEqual(coords.sw({ x: 5, y: 0 }), { x: 4, y: 1 });
         });
     });
 });
