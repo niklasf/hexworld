423a7170d64a5b0202f17439fb28afdfdad9576e
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-07-20 15:22:16 +0200

    Add more terrain graphics and a map fixture.

---
+++ test/fixtures/flatland.js
@@ -0,0 +1,11 @@
+define(function() {
+    return [
+        [ "Gs^Fms", "Gs^Fms", "Gs^Fms", "Gs^Fms", "Wo", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg" ],
+        [ "Wo", "Wo", "Wo", "Wo", "Wo", "Gg", "Gg", "Wo", "Wo", "Gg", "Dd", "Dd" ],
+        [ "Gg", "Wo", "Gg", "Gg", "Wo", "Wo", "Wo", "Gg", "Wo", "Dd", "Dd", "Gs^Fms" ],
+        [ "Gg", "Gg", "Gg", "Gg", "Wo", "Wo", "Wo", "Wo", "Wo", "Dd", "Gg", "Dd" ],
+        [ "Dd", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Wo", "Gg", "Gg", "Gs^Fms", "Gg" ],
+        [ "Dd", "Dd", "Dd", "Gg", "Dd", "Gg", "Dd", "Gg", "Gs^Fms", "Gs^Fms", "Gs^Fms", "Gs^Fms" ],
+        [ "Dd", "Dd", "Dd", "Dd", "Dd", "Dd", "Dd", "Gg", "Gs^Fms", "Gs^Fms", "Gs^Fms", "Gs^Fms" ]
+    ];
+});
