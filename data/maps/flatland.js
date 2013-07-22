2e521ef206ba014cef77036d17a58adeb709a5a2
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-07-22 19:00:43 +0200

    Add windmill.

--- data/maps/flatland.js
+++ data/maps/flatland.js
@@ -1,20 +1,6 @@
 define(function() {
-    var Wo = "Wo";
-    var Gg = "Gg";
-    var Dd = "Dd";
-    var Gs_Fms = "Gs^Fms";
-
     return [
-        [ Gs_Fms, Gs_Fms, Gs_Fms, Gs_Fms, Wo, Gg, Gg, Gg, Gg, Gg, Gg, Gg ],
-        [ Wo, Wo, Wo, Wo, Wo, Gg, Gg, Wo, Wo, Gg, Dd, Dd ],
-        [ Gg, Wo, Gg, Gg, Wo, Wo, Wo, Gg, Wo, Dd, Dd, Gs_Fms ],
-        [ Gg, Gg, Gg, Gg, Wo, Wo, Wo, Wo, Wo, Dd, Gg, Dd ],
-        [ Dd, Gg, Gg, Gg, Gg, Gg, Gg, Wo, Gg, Gg, Gs_Fms, Gg ],
-        [ Dd, Dd, Dd, Gg, Dd, Gg, Dd, Gg, Gs_Fms, Gs_Fms, Gs_Fms, Gs_Fms ],
-        [ Dd, Dd, Dd, Dd, Dd, Dd, Dd, Gg, Gs_Fms, Gs_Fms, Gs_Fms, Gs_Fms ]
-    ];
-
-    return [
+        [ "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gs", "Gg" ],
         [ "Gs^Fms", "Gs^Fms", "Gs^Fms", "Gs^Fms", "Wo", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg" ],
         [ "Wo", "Wo", "Wo", "Wo", "Wo", "Gg", "Gg", "Wo", "Wo", "Gg", "Dd", "Dd" ],
         [ "Gg", "Wo", "Gg", "Gg", "Wo", "Wo", "Wo", "Gg", "Wo", "Dd", "Dd", "Gs^Fms" ],
---
+++ data/maps/human-buildings.js
@@ -0,0 +1,9 @@
+define(function() {
+    return [
+        [ "Gg", "Kh", "Kh", "Kh", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg"],
+        [ "Gg", "Ch", "Ch", "Ch", "Ch", "Gg", "Gg", "Gg", "Gs^Vh", "Gg"],
+        [ "Gg", "Gg", "Ch", "Gg", "Gg", "Gg", "Gs^Vh", "Gg", "Gg", "Gg"],
+        [ "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Gg", "Dd^Vwm", "Gg"],
+        [ "Gg", "Gg", "Gg", "Gg", "Gs^Vh", "Gg", "Gg", "Gs^Vh", "Gg", "Gg" ]
+    ];
+});
