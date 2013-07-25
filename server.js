b2e312d744a7e9d46ea8b2aa58230a5a883e01b7
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-07-25 15:03:14 +0200

    Minify the JavaScript output.

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var requirejs = require("requirejs"),
-    express = require("express");
+    express = require("express"),
+    temp = require("temp");
 
 requirejs.config({ baseUrl: "lib" });
 
 var app = express();
 
-app.use("/vendor", express.static("vendor"));
-app.use("/lib", express.static("lib"));
-
 app.get("/", function (req, res) {
     res.sendfile("index.html");
+});
+
+app.get("/lib/client.js", function (req, res) {
+    var outFile = temp.path({
+        prefix: "hexworld-client.",
+	suffix: ".min.js",
+    });
+    requirejs.optimize({
+        baseUrl: "lib",
+        name: "client",
+        out: outFile,
+        mainConfigFile: "lib/client.js"
+    }, function (buildResponse) {
+        console.log(buildResponse);
+	res.sendfile(outFile);
+    });
 });
 
 app.listen(3000);
